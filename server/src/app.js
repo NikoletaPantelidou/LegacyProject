4b7ea0500de15ab4d9bd38e4cfbed023a263a3c7
AmeneNeon <ameneneon@gamil.com>
2023-07-05 21:58:35 +0300

    create route for book

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,29 @@
-console.log('hello')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes")
+
+// Middlewares
+app.use(express.json())
+app.use("/books",router)
+
+
+
+
+
+mongoose
+  .connect(
+    "mongodb+srv://book:book@books.nclgdh4.mongodb.net/book?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected!");
+  })
+  .then(() => {app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+  });
+  }).catch((err)=>{
+    console.log(err)
+  })
+
+
+
